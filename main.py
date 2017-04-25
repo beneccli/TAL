13529f02867c1a87a84e34e7cdb7e944ad8ecb25
theolaf <theo.lafforgue@gmail.com>
2017-04-25 14:27:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,6 +7,5 @@ from people import People
 People.add(Person("Obama", "Barrack", date.today(), "Missouri", 82, 174, "New York"))
 #People.find("firstname", "Obama")
 
-str = 'Salut ! Connais-tu Barrack Obama ?'
+str = 'slt ! Cmt ça va ?'
 print(ChatBotEngine.talk(str))
-
